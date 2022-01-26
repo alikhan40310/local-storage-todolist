149a3680fa084a7a6432a3ef2bf8ce407c83559c
Ali Khan <alikhan40310@gmail.com>
2022-01-26 20:49:17 +0500

    first changes

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,6 @@
-function blackcolor(){
-    document.body.style.backgroundColor= "black";
-    document.body.style.transition = "0.8s ease";
-    document.body.style.padding = "0";
+function submitFunc(){
+    let valueList = document.createElement("li");
+    let inValue = document.getElementById("inputtext").value;
+    valueList.innerHTML = inValue;
+    document.getElementById("itemList").appendChild(valueList);
 }
-function originalcolor(){
-    
-    document.body.style.backgroundColor= "white";
-    document.body.style.transition = "0.8s ease";
-    document.body.style.padding = "10px";
-
-}
-function addValue(num1,num2){
-    var x = num1 + num2;
-    return x
-
-
-
-}
-
-const total = addValue(5,8)
-const total3 = addValue(55,33)
-console.log(total, total3)
