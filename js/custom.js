bf412ebd37de17e5092f38fccf19873f3f1f35e2
Ali Khan <alikhan40310@gmail.com>
2022-01-27 13:53:49 +0500

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,18 @@
+
 function submitFunc(){
+   
     let valueList = document.createElement("li");
     let inValue = document.getElementById("inputtext").value;
     valueList.innerHTML = inValue;
-    document.getElementById("itemList").appendChild(valueList);
+    document.getElementById("itemList").appendChild(valueList); 
+    document.getElementById("inputtext").value = "";
+
+        localStorage.setItem("listinStorage", inValue);
+        localStorage.getItem("listinStorage");
+
 }
+
+// storing array in local storage
+// let array = [2,4,5,6,7];
+// localStorage.setItem("myArray", array);
+// document.getElementById("demo").innerHTML = localStorage.getItem("myArray");
